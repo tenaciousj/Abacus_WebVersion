9a35016795c90a727a59f68e0ec0c44a2e34e449
jeanettepranin <jeanettepranin@gmail.com>
2016-12-15 21:39:18 -0800

    add one row of beads

--- abacus.js
+++ abacus.js
@@ -1,34 +1,72 @@
-function init() {
+var num_of_beads = 13;
+var objs = [];
+var CANVAS_WIDTH = 73;
+var CANVAS_HEIGHT = 100;
 
-  // Initialize our object
-  obj = {x:50, y:50, w:50, h:50};
-  canvas = document.getElementById("bead");
-
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-
-  // Add eventlistener to canvas
-  canvas.addEventListener('touchmove', function() {
-    //Assume only one touch/only process one touch even if there's more
-    var touch = event.targetTouches[0];
-    
-    // Is touch close enough to our object?
-    if(detectHit(obj.x, obj.y, touch.pageX, touch.pageY, obj.w, obj.h)) {
-      // Assign new coordinates to our object
-      // obj.x = touch.pageX;
-      if(obj.y < touch.pageY){
-        obj.y = Math.min(touch.pageY, 120);
-      } else {
-        obj.y = Math.max(touch.pageY, 50);
-      }
-
-      // Redraw the canvas
-      draw();
-    }
-    event.preventDefault();
-  }, false);
+function init(){
+  var canvas, obj;
+  var rectangle = document.getElementById("rectangle");
+  for(var i = 0; i < num_of_beads; i++){
+    obj = {x:20, y:40, w:60, h:30};
+    objs.push(obj);
+    canvas = document.createElement("canvas");
+    canvas.width = CANVAS_WIDTH;
+    canvas.height = CANVAS_HEIGHT;
+    canvas.id = "canvas"+i;
+    rectangle.appendChild(canvas);
+  }
   draw();
 }
+
+function draw() {
+  var ctx;
+  for(var i = 0; i < num_of_beads; i++){
+    // debugger;
+    canvas = document.getElementById("canvas"+i);
+    ctx = canvas.getContext('2d');
+    
+    // Clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = 'black';
+
+    // Draw our object in its new position
+    ctx.fillRect(objs[i].x, objs[i].y, objs[i].w, objs[i].h);
+  }
+}
+
+// function init() {
+
+//   // Initialize our object
+//   obj = {x:50, y:50, w:50, h:50};
+//   canvas = document.getElementById("bead");
+
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
+
+//   // Add eventlistener to canvas
+//   canvas.addEventListener('touchmove', function() {
+//     //Assume only one touch/only process one touch even if there's more
+//     var touch = event.targetTouches[0];
+    
+//     // Is touch close enough to our object?
+//     if(detectHit(obj.x, obj.y, touch.pageX, touch.pageY, obj.w, obj.h)) {
+//       // Assign new coordinates to our object
+//       // obj.x = touch.pageX;
+//       if(obj.y < touch.pageY){
+//         obj.y = Math.min(touch.pageY, 120);
+//       } else {
+//         obj.y = Math.max(touch.pageY, 50);
+//       }
+
+//       // Redraw the canvas
+//       draw();
+//     }
+//     event.preventDefault();
+//   }, false);
+
+//   draw();
+// }
 
 function detectHit(x1,y1,x2,y2,w,h) {
   //Very simple detection here
@@ -37,28 +75,14 @@ function detectHit(x1,y1,x2,y2,w,h) {
   return true;
 }
 
-function draw() {
-  canvas = document.getElementById("bead");
-  var ctx = canvas.getContext('2d');
-  // Clear the canvas
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
+// function draw() {
+//   canvas = document.getElementById("bead");
+//   var ctx = canvas.getContext('2d');
+//   // Clear the canvas
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  ctx.fillStyle = 'black';
+//   ctx.fillStyle = 'black';
 
-  // Draw our object in its new position
-  ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
-}
-
-// function allowDrop(ev) {
-//     ev.preventDefault();
-// }
-
-// function drag(ev) {
-//     ev.dataTransfer.setData("text", ev.target.id);
-// }
-
-// function drop(ev) {
-//     ev.preventDefault();
-//     var data = ev.dataTransfer.getData("text");
-//     ev.target.appendChild(document.getElementById(data));
+//   // Draw our object in its new position
+//   ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
 // }
